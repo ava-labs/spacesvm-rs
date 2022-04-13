a9dfa7c51fee184710412b03d27f3cfe7c5607f9
Gyuho Lee <gyuho.lee@avalabs.org>
2022-04-13 09:27:11 -0700

    lib.rs: update
    Signed-off-by: Gyuho Lee <gyuho.lee@avalabs.org>

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,15 @@ pub mod plugin;
 pub mod vm {
     tonic::include_proto!("vm");
 }
+use vm::vm_server::Vm;
 
 pub mod metrics {
     tonic::include_proto!("metrics");
 }
+
+#[derive(Debug)]
+pub struct KvVm {}
+
+// impl Vm for KvVm {
+// TODO
+// }
