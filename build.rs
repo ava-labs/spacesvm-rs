effce9a2a320a7fd6b065f9356c64b2e61c3030c
Sam Batschelet <sam.batschelet@avalabs.org>
2022-04-20 14:09:50 -0400

    proto: add required services
    Signed-off-by: Sam Batschelet <sam.batschelet@avalabs.org>

--- build.rs
+++ build.rs
@@ -2,7 +2,21 @@
 fn main() {
     tonic_build::configure()
         .build_server(true)
-        .build_client(false)
-        .compile(&["proto/metrics.proto", "proto/vm.proto"], &["proto"])
+        .build_client(true)
+        .compile(
+            &[
+                "proto/aliasreader.proto",
+                "proto/appsender.proto",
+                "proto/keystore.proto",
+                "proto/messenger.proto",
+                "proto/metrics.proto",
+                "proto/rpcdb.proto",
+                "proto/sharedmemory.proto",
+                "proto/subnetlookup.proto",
+                "proto/vm.proto",
+                "proto/http.proto",
+            ],
+            &["proto"],
+        )
         .unwrap();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,42 @@ pub mod genesis;
 pub mod kvvm;
 pub mod plugin;
 
-pub mod vm {
-    tonic::include_proto!("vm");
+pub mod aliasreaderpb {
+    tonic::include_proto!("aliasreader");
+}
+
+pub mod appsenderpb {
+    tonic::include_proto!("appsender");
+}
+
+pub mod httppb {
+    tonic::include_proto!("http");
+}
+
+pub mod keystorepb {
+    tonic::include_proto!("keystore");
+}
+
+pub mod messengerpb {
+    tonic::include_proto!("messenger");
 }
 
 pub mod metrics {
     tonic::include_proto!("metrics");
+}
+
+pub mod rpcdbpb {
+    tonic::include_proto!("rpcdb");
+}
+
+pub mod sharedmemorypb {
+    tonic::include_proto!("sharedmemory");
+}
+
+pub mod subnetlookuppb {
+    tonic::include_proto!("subnetlookup");
+}
+
+pub mod vmpb {
+    tonic::include_proto!("vm");
 }
