d13c83820e2b2f99cfddcae114eb995650476022
Sam Batschelet <sam.batschelet@avalabs.org>
2022-04-19 21:36:39 -0400

    add basic db methods
    Signed-off-by: Sam Batschelet <sam.batschelet@avalabs.org>

--- src/database.rs
+++ src/database.rs
@@ -2,9 +2,7 @@ use log::info;
 use tonic::transport::Channel;
 use tonic::Status;
 
-use crate::rpcdbpb::{
-    database_client::DatabaseClient, CloseRequest, CloseResponse, GetRequest, PutRequest,
-};
+use crate::rpcdbpb::{database_client::DatabaseClient, *};
 
 pub struct Database {
     client: DatabaseClient<Channel>,
@@ -14,15 +12,18 @@ impl Database {
     pub fn new(client: DatabaseClient<Channel>) -> Self {
         Database { client }
     }
+}
 
-    pub async fn get(&mut self, key: Vec<u8>) -> Result<Option<Vec<u8>>, Status> {
+#[tonic::async_trait]
+impl crate::rpcdbpb::database_server::Database for Database {
+    async fn get(&mut self, key: Vec<u8>) -> Result<Option<Vec<u8>>, Status> {
         let req = GetRequest { key };
         let resp = self.client.get(req).await?.into_inner();
         // TODO: handle db error
         Ok(Some(resp.value))
     }
 
-    pub async fn put(&mut self, key: Vec<u8>, value: Vec<u8>) -> Result<(), Status> {
+    async fn put(&mut self, key: Vec<u8>, value: Vec<u8>) -> Result<(), Status> {
         let req = PutRequest { key, value };
         let resp = self.client.put(req).await?.into_inner();
         info!("db response {:?}", resp);
@@ -30,7 +31,15 @@ impl Database {
         Ok(())
     }
 
-    pub async fn close(&mut self) -> Result<tonic::Response<CloseResponse>, tonic::Status> {
+    async fn delete(&mut self, key: Vec<u8>) -> Result<(), Status> {
+        let req = DeleteRequest { key };
+        let resp = self.client.delete(req).await?.into_inner();
+        info!("db response {:?}", resp);
+        // TODO: handle errors
+        Ok(())
+    }
+
+    async fn close(&mut self) -> Result<tonic::Response<CloseResponse>, tonic::Status> {
         let req = CloseRequest {};
         Ok(self.client.close(req).await?)
     }
