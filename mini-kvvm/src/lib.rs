7d461e096e4bb9c2614a7d39fec8fc11ea79d6c3
Sam Batschelet <sam.batschelet@avalabs.org>
2022-04-30 14:30:27 -0400

    fmt
    Signed-off-by: Sam Batschelet <sam.batschelet@avalabs.org>

--- mini-kvvm/src/lib.rs
+++ mini-kvvm/src/lib.rs
@@ -3,8 +3,3 @@ pub mod engine;
 pub mod genesis;
 pub mod kvvm;
 pub mod plugin;
-
-// pub mod proto {
-//     // gRPC reflection
-//     // pub(crate) const FILE_DESCRIPTOR_SET: &[u8] = tonic::include_file_descriptor_set!("vm"); 
-// }
