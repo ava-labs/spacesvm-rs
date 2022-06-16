78ed6f6d262e1972b8492f8da93214aeeb6a592a
morrisettjohn <morrisettjohn@gmail.com>
2022-06-16 09:48:02 -0400

    again trying to untrack target

--- tests/e2e/target/rls/debug/deps/rand_chacha-4fd276dc9c7482da.d
+++
@@ -1,7 +0,0 @@
-/home/johnmorrisett/Documents/mini-kvvm-rs/mini-kvvm-rs/tests/e2e/target/rls/debug/deps/rand_chacha-4fd276dc9c7482da.rmeta: /home/johnmorrisett/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/lib.rs /home/johnmorrisett/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/chacha.rs /home/johnmorrisett/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/guts.rs
-
-/home/johnmorrisett/Documents/mini-kvvm-rs/mini-kvvm-rs/tests/e2e/target/rls/debug/deps/rand_chacha-4fd276dc9c7482da.d: /home/johnmorrisett/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/lib.rs /home/johnmorrisett/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/chacha.rs /home/johnmorrisett/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/guts.rs
-
-/home/johnmorrisett/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/lib.rs:
-/home/johnmorrisett/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/chacha.rs:
-/home/johnmorrisett/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/guts.rs:
--- tests/e2e/target/rls/debug/deps/tracing_futures-8142bce57ecbeadb.d
+++
@@ -1,7 +0,0 @@
-/home/johnmorrisett/Documents/mini-kvvm-rs/mini-kvvm-rs/tests/e2e/target/rls/debug/deps/tracing_futures-8142bce57ecbeadb.rmeta: /home/johnmorrisett/.cargo/registry/src/github.com-1ecc6299db9ec823/tracing-futures-0.2.5/src/lib.rs /home/johnmorrisett/.cargo/registry/src/github.com-1ecc6299db9ec823/tracing-futures-0.2.5/src/stdlib.rs /home/johnmorrisett/.cargo/registry/src/github.com-1ecc6299db9ec823/tracing-futures-0.2.5/src/executor/mod.rs
-
-/home/johnmorrisett/Documents/mini-kvvm-rs/mini-kvvm-rs/tests/e2e/target/rls/debug/deps/tracing_futures-8142bce57ecbeadb.d: /home/johnmorrisett/.cargo/registry/src/github.com-1ecc6299db9ec823/tracing-futures-0.2.5/src/lib.rs /home/johnmorrisett/.cargo/registry/src/github.com-1ecc6299db9ec823/tracing-futures-0.2.5/src/stdlib.rs /home/johnmorrisett/.cargo/registry/src/github.com-1ecc6299db9ec823/tracing-futures-0.2.5/src/executor/mod.rs
-
-/home/johnmorrisett/.cargo/registry/src/github.com-1ecc6299db9ec823/tracing-futures-0.2.5/src/lib.rs:
-/home/johnmorrisett/.cargo/registry/src/github.com-1ecc6299db9ec823/tracing-futures-0.2.5/src/stdlib.rs:
-/home/johnmorrisett/.cargo/registry/src/github.com-1ecc6299db9ec823/tracing-futures-0.2.5/src/executor/mod.rs:
